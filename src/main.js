30a4c90ccd851384a1df35002f5b2979d890fed4
Hasan DAĞ <hdag@havelsan.com.tr>
2018-09-26 16:25:07 +0300

    my message

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
+//import App from './App.vue'
+import Table1 from './Table1.vue'
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue);
 
 new Vue({
-  el: '#app',
+  el: '#app2',
   render: h => h(App)
 })
